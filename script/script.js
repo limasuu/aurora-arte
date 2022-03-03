eb2d8117b94d33053cc0159715bc26321a47118e
limasuu <szlima.93@gmail.com>
2022-03-03 11:46:27 -0300

    Adicionando carrossel de cursos no index (básico)

---
+++ script/script.js
@@ -0,0 +1,17 @@
+var num= 1;
+                    
+setInterval(function(){
+
+    document.getElementById('radio'+num).checked= true; 
+                                            
+    num++;
+
+    if(num > 5)
+        num= 1;
+}, 5000);
+
+
+
+function attContagemSlide(botao){
+    num= botao.id[3];
+}

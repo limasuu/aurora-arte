101c2f9704ebbdafbdb519057dde4a7c82e6b06e
limasuu <szlima.93@gmail.com>
2022-03-30 14:20:21 -0300

    Adicionando evento de toque no menu compacto

--- script/script-menu.js
+++ script/script-menu.js
@@ -1,12 +1,18 @@
 const iconeCompactoBotao= document.querySelector("#icone-menu-compacto");
-iconeCompactoBotao.addEventListener('click', () => {
-    
+
+function abrirMenuCompacto(evento){
+    if(evento.type === 'touchstart')
+        evento.preventDefault();
+
     var nav= document.querySelector("nav");
     nav.classList.toggle("lista-menu-compacto");
-
+    
     const body= document.querySelector("body");
     body.classList.toggle("desabilita-scroll");
-
+    
     iconeCompactoBotao.classList.toggle("fa-bars");
     iconeCompactoBotao.classList.toggle("fa-x");
-});
+}
+
+iconeCompactoBotao.addEventListener('click', abrirMenuCompacto);
+iconeCompactoBotao.addEventListener('touchstart', abrirMenuCompacto);
